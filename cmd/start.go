e61f74cdecd15da946c3682232174937ce96ca9d
mdxabu <abdullahfakrudeen2020@gmail.com>
2025-07-11 21:35:36 +0530

    Refactor start command to improve output message placement and formatting

--- cmd/start.go
+++ cmd/start.go
@@ -16,14 +16,19 @@ var startCmd = &cobra.Command{
 	Short: "Command to start the ipscout process",
 	Long:  `Command to start the ipscout process. This command will start the ipscout process and begin monitoring the network.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Process started, This may take a few seconds to start monitoring the network.")
-
 		useMonitor, _ := cmd.Flags().GetBool("monitor")
 		useIPv4, _ := cmd.Flags().GetBool("ipv4")
 		useIPv6, _ := cmd.Flags().GetBool("ipv6")
-		filterSrcIP, _ := cmd.Flags().GetString("srcip") 
+		filterSrcIP, _ := cmd.Flags().GetString("srcip")
+
+
+		if (useIPv4 && useIPv6) || (!useIPv4 && !useIPv6) {
+			fmt.Println("Error: You must specify exactly one of --ipv4 or --ipv6.")
+			return
+		}
 
 		if useMonitor {
+			fmt.Println("Process started, This may take a few seconds to start monitoring the network.")
 			fmt.Println("Starting packet sniffing...")
 
 			devices, err := pcap.FindAllDevs()
@@ -59,5 +64,5 @@ func init() {
 	startCmd.Flags().BoolP("output", "d", false, "output the network data")
 	startCmd.Flags().BoolP("ipv4", "4", false, "output the IPv4 data")
 	startCmd.Flags().BoolP("ipv6", "6", false, "output the IPv6 data")
-	startCmd.Flags().String("srcip", "", "filter packets by source IP address") // Add this line
+	startCmd.Flags().String("srcip", "", "filter packets by source IP address")
 }
